3dbd49263639eea4973b356ae2237fb662508811
inflamcucum <93724130+inflamcucum@users.noreply.github.com>
2021-11-23 17:50:11 +0800

    Update Residential.h

--- buildings/Residential.h
+++ buildings/Residential.h
@@ -10,9 +10,13 @@ public:
     virtual int get_population_growth() const override;
     virtual int get_population_growth_rate_contribution() const override;
     virtual void increase_population(int population) override;
+    virtual int update_work_trips() override;
+    virtual int update_health_trips() override;
 
 protected:
     int population;
+    int work_trips;
+    int health_trips;
     explicit Residential(City &city, int population);
 };
 #endif // RESIDENTIAL_H
