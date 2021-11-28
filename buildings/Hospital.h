957f7c47a8982fca3e2d4f414ee7d6220a5bd972
inflamcucum <93724130+inflamcucum@users.noreply.github.com>
2021-11-29 05:26:57 +0800

    Update Hospital.h

--- buildings/Hospital.h
+++ buildings/Hospital.h
@@ -5,10 +5,12 @@
 class Hospital: public Health {
 public:
     static const int cost{500};
+    static const int attractiveness{5};
 
     explicit Hospital(City &city);
     virtual Node::Type get_type() const override;
     virtual int get_cost() const override;
+    virtual int get_attractiveness() const override;
 
     virtual int get_population_growth_rate_contribution() const override;
 
