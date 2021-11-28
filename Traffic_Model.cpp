50d825b1259fdc44fe85fcb914bd005fe6f4c90f
inflamcucum <93724130+inflamcucum@users.noreply.github.com>
2021-11-29 05:05:01 +0800

    Add files via upload
    I also include step 2 d code inside Traffic_Model here

--- Traffic_Model.cpp
+++ Traffic_Model.cpp
@@ -1,51 +1,206 @@
 #include "Traffic_Model.h"
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void Trip_Assignment::trip_assignment_main(){
-    
-    /// Error checking
-    if(size_checking()){
-        return;
-    }
 
-    /// loops
-    for(int i = 0; i < origin.size(); ++i){
-        for(int j = 0; j < destination.size(); ++j){
-            int trips = OD_Matrix[i][j];
-            std::vector<std::vector<Road*>> paths = get_all_paths(origin[i], destination[j]);
-            for(int k = 0; k < trips; k+= incremental_amount){
-                load_traffic(paths);  
+/**
+ * The following set functions maybe move to city's class
+
+Trip_Distribution::Trip_Distribution(City &city):city(city){
+    Trip_Distribution::set_all_residential();
+    Trip_Distribution::set_all_revenue();
+    Trip_Distribution::set_all_health();
+}
+
+void Trip_Distribution::set_all_residential(){
+    for(int i=0; i<city.get_grid_size(); ++i){
+        for(int j=0; j<city.get_grid_size(); ++j){
+            if(city.grid[i][j] != nullptr){
+                if(city.grid[i][j]->get_category() == Node::Category::RESIDENTIAL){
+                    all_residential_building.push_back(dynamic_cast<Residential*>(city.grid[i][j]));
+                }
             }
         }
     }
-
-    /// old version of code using iterator
-    /**
-    std::vector<Road*>::iterator p;
-    int i = 0;
-    for(p = origin.begin(); p != origin.end(); ++p, ++i){
-        std::vector<Road*>::iterator q;
-        int j = 0;
-        for(q = destination.begin(); q != destination.end(); ++q, ++j){
-            int trips = OD_Matrix[i][j];
-            std::vector<std::vector<Road*>> paths = get_all_paths(*p, *q);
-            for(int k = 0; k < trips; ++k){
-                load_traffic(paths);  
-            }                    
-        }
-    }
-    */
 }
 
-int Trip_Generation::find_shortest_path(Road* start_pt, Road* end_pt, std::vector<Road*>& path){
-    
+void Trip_Distribution::set_all_revenue(){
+    for(int i=0; i<city.get_grid_size(); ++i){
+        for(int j=0; j<city.get_grid_size(); ++j){
+            if(city.grid[i][j] != nullptr){
+                if(city.grid[i][j]->get_category() == Node::Category::REVENUE){
+                    all_destination_building.push_back(dynamic_cast<Revenue*>(city.grid[i][j]));
+                }
+            }
+        }
+    }
+}
 
-    path.push_back(start_pt);
-    /// handling path to make sure it contains the current road*
-    //if(path.size() == 0){path.push_back(start_pt);}
-    //else if(path.back() != start_pt) path.push_back(start_pt);
+void Trip_Distribution::set_all_health(){
+    for(int i=0; i<city.get_grid_size(); ++i){
+        for(int j=0; j<city.get_grid_size(); ++j){
+            if(city.grid[i][j] != nullptr){
+                if(city.grid[i][j]->get_category() == Node::Category::HEALTH){
+                    all_destination_building.push_back(dynamic_cast<Health*>(city.grid[i][j]));
+                }
+            }
+        }
+    }
+}
+*/
+
+Trip_Distribution::Trip_Distribution(City &city, std::vector<Residential*> & residential_buildings,
+        std::vector<Revenue*> & revenue_buildings, std::vector<Health*> & health_buildings):
+        city(city),all_residential_building(residential_buildings),
+        all_revenue_building(revenue_buildings), all_health_building(health_buildings){}
+
+bool Trip_Distribution::trip_distribution_main(Node::Category category){
+    /// No need for distribution and assignment
+    if(all_residential_building.size() == 0 || category == Node::Category::RESIDENTIAL || category == Node::Category::ROAD)
+        return false;
+
+    /// Error check: this function should not be called twice
+    if(origin.size() != 0 || destination.size() != 0 || all_destination_building.size() != 0) return false;
+    set_origin();
+    if(category == Node::Category::REVENUE){
+        if(all_revenue_building.size() == 0 ) return false; /// No need for distribution and assignment
+        set_work_destination();
+    }else if(category == Node::Category::HEALTH){
+        if(all_health_building.size() == 0 ) return false; /// No need for distribution and assignment
+        set_health_destination();
+    } else {return false;}
+
+    /// Error check: check for size consistency
+    if( all_residential_building.size() != origin.size() ||
+        (all_revenue_building.size() != all_destination_building.size() && all_health_building.size() != all_destination_building.size())  ||
+        all_destination_building.size() != destination.size()
+        )return false;
+    
+    /// Error check: this function should not be called twice
+    if(factors.size() == 0 || OD_path.size() == 0 || OD_matrix.size() == 0) return false;
+    set_factor_and_OD_path();
+    set_OD_matrix(category);
+    return true;    
+}
+
+void Trip_Distribution::set_origin(){
+    for(std::vector<Residential*>::iterator i = all_residential_building.begin(); i != all_residential_building.end(); i++){
+        Residential* residential = *i;
+        if(residential->is_neighbor_road(Node::Direction::NORTH) == true){
+            origin.push_back(dynamic_cast<Road*>(residential->get_neighboring_node(Node::Direction::NORTH)));
+        }
+        else if(residential->is_neighbor_road(Node::Direction::EAST) == true){
+            origin.push_back(dynamic_cast<Road*>(residential->get_neighboring_node(Node::Direction::EAST)));
+        }
+        else if(residential->is_neighbor_road(Node::Direction::SOUTH) == true){
+            origin.push_back(dynamic_cast<Road*>(residential->get_neighboring_node(Node::Direction::SOUTH)));
+        }
+        else if(residential->is_neighbor_road(Node::Direction::WEST) == true){
+            origin.push_back(dynamic_cast<Road*>(residential->get_neighboring_node(Node::Direction::WEST)));
+        }else{
+            origin.push_back(nullptr);
+        }
+    }
+}
+
+void Trip_Distribution::set_work_destination(){
+    // all_destination_building = static_cast<std::vector<Node*>>(all_revenue_building);
+    for(std::vector<Revenue*>::iterator i = all_revenue_building.begin(); i != all_revenue_building.end(); i++){
+        Revenue* revenue = *i;
+        all_destination_building.push_back(revenue);
+        if(revenue->is_neighbor_road(Node::Direction::NORTH) == true){
+            destination.push_back(dynamic_cast<Road*>(revenue->get_neighboring_node(Node::Direction::NORTH)));
+        }
+        else if(revenue->is_neighbor_road(Node::Direction::EAST) == true){
+            destination.push_back(dynamic_cast<Road*>(revenue->get_neighboring_node(Node::Direction::EAST)));
+        }
+        else if(revenue->is_neighbor_road(Node::Direction::SOUTH) == true){
+            destination.push_back(dynamic_cast<Road*>(revenue->get_neighboring_node(Node::Direction::SOUTH)));
+        }
+        else if(revenue->is_neighbor_road(Node::Direction::WEST) == true){
+            destination.push_back(dynamic_cast<Road*>(revenue->get_neighboring_node(Node::Direction::WEST)));
+        }else{
+            destination.push_back(nullptr);
+        }
+    }
+}
+
+void Trip_Distribution::set_health_destination(){
+    // all_destination_building = static_cast<std::vector<Node*>>(all_health_building);
+    for(std::vector<Health*>::iterator i = all_health_building.begin(); i != all_health_building.end(); ++i){
+        Health* health = *i;
+        all_destination_building.push_back(health);
+        if(health->is_neighbor_road(Node::Direction::NORTH) == true){
+            destination.push_back(dynamic_cast<Road*>(health->get_neighboring_node(Node::Direction::NORTH)));
+        }
+        else if(health->is_neighbor_road(Node::Direction::EAST) == true){
+            destination.push_back(dynamic_cast<Road*>(health->get_neighboring_node(Node::Direction::EAST)));
+        }
+        else if(health->is_neighbor_road(Node::Direction::SOUTH) == true){
+            destination.push_back(dynamic_cast<Road*>(health->get_neighboring_node(Node::Direction::SOUTH)));
+        }
+        else if(health->is_neighbor_road(Node::Direction::WEST) == true){
+            destination.push_back(dynamic_cast<Road*>(health->get_neighboring_node(Node::Direction::WEST)));
+        }else{
+            destination.push_back(nullptr);
+        }
+    }
+}
+
+
+void Trip_Distribution::set_factor_and_OD_path(){
+    for(int i=0; i<origin.size(); i++){
+        /// Calculate factors using gravity model
+        /// call find_shortest_path and store store the respective shortest path in OD_path
+        factors.push_back(std::vector<float>());
+        OD_path.push_back(std::vector<vector<Road*>>());
+        for(int j=0; j<destination.size(); j++){
+            std::vector<Road*>path;
+            if(origin[i] == nullptr || destination[j] == nullptr){
+                factors[i].push_back(0);
+                OD_path[i].push_back(vector<Road*>());
+            }
+            float dist = find_shortest_path(origin[i],destination[j],path);
+            if(dist >= max_path){
+                factors[i].push_back(0);
+                OD_path[i].push_back(vector<Road*>());
+            }else if(dist >= 0){
+                factors[i].push_back(all_destination_building[j]->get_attractiveness()/dist);
+                OD_path[i].push_back(path);
+            }else{
+                cerr<<"Error in Trip_Distribution::find_shortest_path: return 0 or -ve number"<<endl;
+                exit(-1);
+            }
+        }
+
+        /// Change factors to weight
+        float total_factors = 0;
+        for(int j=0; j<factors[i].size(); ++j){
+            total_factors += factors[i][j];
+        }
+        if(total_factors == 0) continue;
+        for(int j=0; j<factors[i].size(); ++j){
+            factors[i][j] = factors[i][j]/total_factors;
+        }
+    }
+}
+
+int Trip_Distribution::find_shortest_path(Road* start_pt, Road* end_pt){
+    std::vector<Road*> temp;
+    return find_shortest_path(start_pt, end_pt, temp);
+}
+
+int Trip_Distribution::find_shortest_path(Road* start_pt, Road* end_pt, std::vector<Road*>& path){
+
+    /// handling path to make sure it contains the current road*    
+    if(path.size() == 0){
+        path.push_back(start_pt);
+    }else{
+        if(path.back() != start_pt)
+            path.push_back(start_pt);
+    }
 
     /// end cases
     /// case 1: reach destination
@@ -142,6 +297,72 @@ int Trip_Generation::find_shortest_path(Road* start_pt, Road* end_pt, std::vecto
     return path_dist;   
 
 }
+
+
+void Trip_Distribution::set_OD_matrix(Node::Category category){
+
+    for(int i = 0; i < factors.size(); ++i){
+        OD_matrix.push_back(std::vector<int>());
+
+        int total_trips = 0;
+
+        /// get total number of trips, depending on category
+        if(category == Node::Category::REVENUE){
+            total_trips = all_residential_building[i]->get_work_trips();
+        }else if(category == Node::Category::HEALTH){
+            total_trips = all_residential_building[i]->get_health_trips();
+        }else{
+            cerr<<"Weird category input in Trip_Distribution::OD_matrix_traffic"<<endl;
+            exit(-1);
+        }
+
+        /// set OD_matrix
+        for(int j=0; j<factors[i].size(); ++j){
+            OD_matrix[i].push_back(total_trips*factors[i][j]);
+        }
+    }
+}
+
+
+void Trip_Assignment::trip_assignment_main(){
+    
+    /// Error checking
+    if(size_checking()){
+        return;
+    }
+
+    /// loops
+    for(int i = 0; i < origin.size(); ++i){
+        if(origin[i] == nullptr) continue;
+        for(int j = 0; j < destination.size(); ++j){
+            if(destination[j] == nullptr) continue;
+            int trips = OD_Matrix[i][j];
+            if(trips == 0) continue;
+            std::vector<std::vector<Road*>> paths = get_all_paths(origin[i], destination[j]);
+            for(int k = 0; k < trips; k+= incremental_amount){
+                load_traffic(paths);  
+            }
+        }
+    }
+
+    /// old version of code using iterator
+    /**
+    std::vector<Road*>::iterator p;
+    int i = 0;
+    for(p = origin.begin(); p != origin.end(); ++p, ++i){
+        std::vector<Road*>::iterator q;
+        int j = 0;
+        for(q = destination.begin(); q != destination.end(); ++q, ++j){
+            int trips = OD_Matrix[i][j];
+            std::vector<std::vector<Road*>> paths = get_all_paths(*p, *q);
+            for(int k = 0; k < trips; ++k){
+                load_traffic(paths);  
+            }                    
+        }
+    }
+    */
+}
+
 
 std::vector<std::vector<Road*>> Trip_Assignment::get_all_paths(Road* start_pt, Road* end_pt){
     std::vector<std::vector<Road*>> paths;
--- Traffic_Model.h
+++ Traffic_Model.h
@@ -4,62 +4,162 @@
 #include "buildings/GoldMine.h"
 #include "buildings/House.h"
 #include "buildings/Apartment.h"
-
-/**
- * The following code should be incorperatred into the city's class, probably
- */ 
-
-
-vector<Residential*> all_residential_buildings;
-vector<Revenue*> all_revenue_buildings;
-vector<Health*> all_health_buildings;
-
-
-/**
- * The following code should remained here
- * This source code should be involving all global functions that handle the OD Matrix
- * The initialisation of the OD_Maxtrix should be handled outside this source code
- */ 
+#include "Node.h"
+#include "buildings/Road.h"
 
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-#include "City.h"
-#include "Node.h"
-#include "buildings/Road.h"
+/**
+ * In this project, our group implements the Sequential Demand Forecasting Model introduced in CIVL3610 Traffic and
+ * Transportation Engineering of HKUST to predict traffic flow and respective travel time in a city.
+ * 
+ * The Sequential Demand Focasting Model consists of four steps:
+ * Step 1: Trip Generation (Decision: to travel or not)
+ * Step 2: Trip Distrubution (Decision – to where, what destination?)
+ * Step 3: Modal Split (Decision – how, what mode?)
+ * Step 4: Traffic Assignment (Decision – how, which route?)
+ * 
+ * In this project, these four steps are implemented by the following parts of our code respectively:
+ * Step 1: member functions of Residential class
+ * Step 2: Trip_Distrubution class in this file
+ * Step 3: This step is skiped since we do not consider multiple travel modes in the city (e.g. MTR/bus)
+ * Step 4: Trip_Assignment class in this file
+ */
 
+class City;
 
-class Trip_Generation {
-    
-    public:
-        int find_shortest_path(Road* start_pt, Road* end_pt, std::vector<Road*>& paths);
-        static const int max_path{999};
-};
+/**
+ * Trip_Distribution is a class that perform the second step of the sequential demand focasting model.
+ * 
+ * Gravity model is used as the trip distribution model in our project.
+ * Q(I,J) = k*P(I)*A(J)/W(I,J)^c
+ * where    Q(I,J) is the traffic flow from origin I to destination J
+ *          P(I) is the number of trip production at origin I 
+ *          A(J) is the attractiveness of destination J
+ *          W(I,J) is a measure of the predetermined travel time/ distance between I and J
+ *          c   is a parameter obtained from calibration of real life data. Here we set c = 1 as a dummy variable
+ *              for the SimpleCity game
+ *          k   is a parameter that can be eliminated by applying the trip-production balance constraints:
+ *                  P(I) = sum of Q(I,J) over all J
+ * 
+ * An OD matrix (origin-destination matrix) is defined as a matrix storing Q(I,J) for all I,J.
+ * In our code, a 2D vector of int is used to be the OD matrix.
+ * To provide more flexibility for the user, we also define a 2D vector of vector<Road*> to stores the shortest
+ * path from origin to destination.
+ * 
+ * We define City as the friend class of this class for City to get the results of Trip Distribution.
+ */
 
-
+/*** Note by Jacky: I have change some of the implementation of the code so the following discription is inaccurate now
+Trip_Distribution is an object used to generate 3 vectors, which include origin matrix, destination matrix, and OD number of trips 2d vector. 
+First, 3 vectors containing residential, revenue, and health buildings are generated. Then, according to the category of trips, including
+home-work trips and home-health trips, it will call corresponding set origin and set destination function to set origin and destination vector. 
+After that, travel time matrix and number of trips matrix is generated one after he other. 
+***/
 
 /**
  * WARNING
- * This class is designed to handle Road* instead of Node*
- * DO NOT PASS IN ANY VECTOR OF Node* TO THE OBJECTS OF THIS CLASS
+ * This class is designed to perform trip distribution for one set of OD only.
+ * Create a seperate object for another set of OD.
+ * DO NOT call the trip_distribution_main function more than once.
  */
+
+class Trip_Distribution{
+    friend class City;
+    private:
+        std::vector<Residential*> & all_residential_building;
+        std::vector<Revenue*> & all_revenue_building;
+        std::vector<Health*> & all_health_building;
+
+        std::vector<Node*> all_destination_building;
+        std::vector<Road*> origin; /// store nullptr if the residential building does not have road nearby
+        std::vector<Road*> destination; /// store nullptr if the revenue/health building does not have road nearby
+
+        std::vector<vector<float>> factors; /// store 0 if their is no path if there is no possible path from the origin to destination
+                                            /// This is a temporatory 2D vector used for the intermediate steps of the 
+        std::vector<vector<vector<Road*>>> OD_path; /// store EMPTY vector<Road*> if there is no possible path from the origin to destination
+        std::vector<vector<int>> OD_matrix; /// store 0 if their is no path if there is no possible path from the origin to destination
+        City& city;
+        
+
+    public:
+        /// Regression parameter used in gravity model
+        static const float c{1.0f};
+        static const int max_path{999};
+
+
+        /// constructor
+        explicit Trip_Distribution(City &city, std::vector<Residential*> & residential_buildings,
+        std::vector<Revenue*> & revenue_buildings, std::vector<Health*> & health_buildings);
+
+        /// Main function used for trip distrubution
+        /// return true only if this function is correctly implemented
+        /// return false if there is no origin/destination for trip distribution, or the code is wrongly implmented
+        /// Call this function ONCE only for each object
+        bool trip_distribution_main(Node::Category category);
+
+        /// Remaining are some helper functions that needed for trip assignment 
+
+        /**
+         * These three functions set the origin and destination data member of this class.
+         * Warning: nullptr will be set if there is no road near the residential/commercial/health buildings
+         */ 
+        void set_origin();
+        void set_work_destination();
+        void set_health_destination();
+        void set_factor_and_OD_path();
+        void set_OD_matrix(Node::Category category);
+
+        /** 
+         * Function that returns the number of road that needed to go from start_pt to end_pt, as well as a vector containing
+         * all the road pointers that point to the road objects in this path
+         * Pass an EMPTY vector<Road*> to this function for it to work
+         * Note that for the gravity model to hold, this function will return 1 if start_pt = end_pt
+         * Return max_path if it is impossible to go from start_pt to end_pt
+         */
+        int find_shortest_path(Road* start_pt, Road* end_pt, std::vector<Road*>& path);
+        int find_shortest_path(Road* start_pt, Road* end_pt); /// an overloading function for correct implementation without path
+
+};
+
+/**
+ * Trip_Assignment is a class that perform the fourth step of the sequential demand focasting model.
+ * 
+ * Incremental Assignment is used in this class, in which:
+ * -    A portion of the O-D entries is assigned at each iteration.
+ * -    Travel times are then updated and an additional portion of the O-D matrix is loaded onto the network.
+ * 
+ * Part of the codes used in this step are implemented in the Road class.
+ * 
+ * We define City as the friend class of this class for City to get the results of Trip Assignment.
+ */
+
+/**
+ * WARNINGS
+ * 1.   This class is designed to handle Road* instead of Node*
+ *      DO NOT PASS IN ANY VECTOR OF Node* TO THE OBJECTS OF THIS CLASS
+ * 2.   This class can MODIFY the data members in Road objects
+ */
+
+
 class Trip_Assignment {
     
     private:
-
-        std::vector<Road*> origin;
-        std::vector<Road*> destination;
-        std::vector<std::vector<int>> OD_Matrix;
+        std::vector<Road*>& origin;
+        std::vector<Road*>& destination;
+        std::vector<std::vector<int>>& OD_Matrix;
+        std::vector<std::vector<float>> OD_travel_time;
         City &city;
+        friend class City;
     public:
 
         /// The incremental portion that used in trip assignment
         static const int incremental_amount{1};        
 
-        /// constructors and functions that setup data members
-        explicit Trip_Assignment();
-        explicit Trip_Assignment(std::vector<Road*> origin, std::vector<Road*> destination, std::vector<std::vector<int>> OD_Matrix);
+        /// constructors and function that setup data members
+        explicit Trip_Assignment(std::vector<Road*>& origin, std::vector<Road*>& destination, std::vector<std::vector<int>>& OD_Matrix);
         void set_Traffic_Model(std::vector<Road*> origin, std::vector<Road*> destination, std::vector<std::vector<int>> OD_Matrix);
 
         /// Main functions used for trip assigment
