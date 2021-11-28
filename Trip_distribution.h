e0bc3316c960a4773103e69c966fb775ace2408c
146snk <91668969+146snk@users.noreply.github.com>
2021-11-29 00:56:16 +0800

    description to trip distribution
    added description to trip distribution

--- Trip_distribution.h
+++ Trip_distribution.h
@@ -1,3 +1,9 @@
+/***
+Trip_distribution is an object used to generate 3 vectors, which include origin matrix, destination matrix, and OD number of trips 2d vector. 
+First, 3 vectors containing residential, revenue, and health buildings are generated. Then, according to the category of trips, including
+home-work trips and home-health trips, it will call corresponding set origin and set destination function to set origin and destination vector. 
+After that, travel time matrix and number of trips matrix is generated one after he other. 
+***/
 #ifndef TRIP_DISTRIBUTION_H
 #define TRIP_DISTRIBUTION_H
 
@@ -42,3 +48,11 @@ class Trip_distribution{
 
 
 #endif //TRIP_DISTRIBUTION_H
+
+
+/**********
+at each turn, first create an object Trip_distibution home_work, then call Trip_distribution::set_origin() and 
+Trip_distribution::set_work_destination(), then call Trip_distribution::OD_matrix_travel_time(), 
+then call OD_matrix_traffic(Node::Category::REVENUE). Repeat the above, but change work to health, and REVENUE to HEALTH. 
+The resulting is std::vector<Node*> origin, std::vector<Node*> destination, and std::vector<std::vector<int>> OD_matrix_number_of_trips. 
+**********/ 
