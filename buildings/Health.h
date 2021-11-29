7fa260dfdaa860a1bb7660f067dd5549378ccb57
Yoyo Mok <72611673+nymok@users.noreply.github.com>
2021-11-30 00:20:44 +0800

    Update Health.h

--- buildings/Health.h
+++ buildings/Health.h
@@ -3,6 +3,15 @@
 
 #include "Node.h"
 
+/* *
+* Health Class
+* 
+* This class holds information and methods for Health nodes in the city grid. 
+* This class inherites the Node class. 
+*
+* */
+
+
 class Health: public Node {
 public:
     virtual Node::Category get_category() const override;
