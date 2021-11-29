f8509543b97c3d3a87a8d4a22bf2c2605f2f47f2
Yoyo Mok <72611673+nymok@users.noreply.github.com>
2021-11-30 01:12:14 +0800

    Update Street.h

--- buildings/Street.h
+++ buildings/Street.h
@@ -5,7 +5,7 @@
 class Street: public Road {
     public:
         static const int cost{20};
-        static constexpr double max_traffic_flow{200};
+        static constexpr double max_traffic_flow{100};
 
         // constructor
         explicit Street(City &city);
